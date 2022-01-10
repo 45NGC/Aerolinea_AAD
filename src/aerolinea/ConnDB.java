eeccf66567366c34569dfaff04c696c2130d7e52
45NGC <79377468+45NGC@users.noreply.github.com>
2022-01-10 17:15:53 +0100

    Proyecto por defecto

---
+++ src/aerolinea/ConnDB.java
@@ -0,0 +1,49 @@
+package aerolinea;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author angel
+ */
+public class ConnDB {
+
+    private Connection conn;
+    private ResultSet rs;
+
+    /**
+     * Constructor por defecto que se conecta a una base de datos especifica
+     */
+    public ConnDB() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/aerolinea", "root", "");
+            System.out.println("Conexion con la base de datos!");
+        } catch (SQLException ex) {
+            System.out.println("No hay conexion con la base de datos");
+        }
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void cargaDatosPasajeros() {
+        Statement stmt = null;
+        String query = "SELECT * FROM Pasajeros";
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
